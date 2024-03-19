1d7de3fd35eba827cb0b85d132e4626c5b54fda3
Sandy Pirôpo <121200148+sandypiropo@users.noreply.github.com>
2024-03-19 17:35:16 -0300

    init

---
+++ src/main/java/com/api/findcep/FindcepApplication.java
@@ -0,0 +1,13 @@
+package com.api.findcep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindcepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindcepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/findcep/FindcepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.findcep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindcepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
